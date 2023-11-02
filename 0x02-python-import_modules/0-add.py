a50335bff5970c0e469ec1fcee7306256baca826
dopAmine <amineelmkiddem0@gmail.com>
2023-11-02 12:54:24 -0700

    task

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,12 @@
+# Define variables a and b
+a = 1
+b = 2
+
+# Import the add function from add_0.py
+from add_0 import add
+
+# Calculate the result using the add function
+result = add(a, b)
+
+# Print the formatted string
+print("{} + {} = {}".format(a, b, result))
