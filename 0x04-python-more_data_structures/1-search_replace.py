0542bd9e66399383d39299d1693c8c2f744e8c91
dopAmine <amineelmkiddem0@gmail.com>
2023-11-09 07:33:59 -0800

    task

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = list(map(lambda x: replace if x == search else x, my_list))
-    return (new_list)#!/usr/bin/python3
-def search_replace(my_list, search, replace):
-        new_list = list(map(lambda x: replace if x == search else x, my_list))
-            return (new_list)
+    return (new_list)
